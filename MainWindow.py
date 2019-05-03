5d5b68adf586e8756829a6a33c785c875cea7741
DiaosSama <38740934+DiaosSama@users.noreply.github.com>
2019-05-03 16:04:22 +0800

    5-3：正式版Ver1.0.4
    更新用户表缓存对接

--- MainWindow.py
+++ MainWindow.py
@@ -14,6 +14,7 @@ import shortest
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, usrname):
         self.usrname = usrname
+        self.usrHashTable = uT.usrsHashTable()
         super().__init__()
         self.init_ui()
         self.init_connect()
@@ -451,7 +452,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.personal_layout = QtWidgets.QGridLayout()
 
         # 获取用户信息
-        self.User = uT.usrsHashTable().get(self.usrname)[1]
+        # self.User = uT.usrsHashTable().get(self.usrname)[1]
+        self.User = self.usrHashTable.get(self.usrname)[1]
 
         # 头像
         self.head = QtWidgets.QLabel()
@@ -1203,7 +1205,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def freshUserFlight(self):
         # 更新乘客航班信息
-        self.User = uT.usrsHashTable().get(self.usrname)[1]
+        # self.User = uT.usrsHashTable().get(self.usrname)[1]
+        self.User = self.usrHashTable.get(self.usrname)[1]
         self.UserFlight = self.User["flightInfo"]
         self.real_name_text.setText(self.User["realName"])
         self.age_text.setText(str(self.User["age"]))
@@ -1538,14 +1541,16 @@ class MainWindow(QtWidgets.QMainWindow):
                 ticket["remTicketNum"] -= 1
                 newTicket = FlightInfo(ticket)
                 revFlightInfo(oldTicket, newTicket)
-                uT.usrsHashTable().addFlightInfo(name, ticket)
+                # uT.usrsHashTable().addFlightInfo(name, ticket)
+                self.usrHashTable.addFlightInfo(name, ticket)
                 return Const.SUCCESS
             else:
                 oldTicket = FlightInfo(ticket)
                 ticket["waitingList"].append(name)
                 newTicket = FlightInfo(ticket)
                 revFlightInfo(oldTicket, newTicket)
-                uT.usrsHashTable().addFlightInfo(ticket)
+                # uT.usrsHashTable().addFlightInfo(ticket)
+                self.usrHashTable.addFlightInfo(ticket)
                 return Const.TICKET_WAITING
         else:
             return Const.TICKET_ORDERED  # 不可重复订票
@@ -1591,21 +1596,25 @@ class MainWindow(QtWidgets.QMainWindow):
                 ticket["orderedList"].append(waitname)
                 newTicket = FlightInfo(ticket)
                 revFlightInfo(oldTicket, newTicket)
-                uT.usrsHashTable().deleteFlightInfo(name, ticket)
-                uT.usrsHashTable().reviseFlightInfo(waitname, oldTicket.__dict__, ticket)
+                # uT.usrsHashTable().deleteFlightInfo(name, ticket)
+                # uT.usrsHashTable().reviseFlightInfo(waitname, oldTicket.__dict__, ticket)
+                self.usrHashTable.deleteFlightInfo(name, ticket)
+                self.usrHashTable.reviseFlightInfo(waitname, oldTicket.__dict__, ticket)
                 return Const.SUCCESS
             else:
                 ticket["remTicketNum"] += 1
                 newTicket = FlightInfo(ticket)
                 revFlightInfo(oldTicket, newTicket)
-                uT.usrsHashTable().deleteFlightInfo(name, ticket)
+                # uT.usrsHashTable().deleteFlightInfo(name, ticket)
+                self.usrHashTable.deleteFlightInfo(name, ticket)
                 return Const.SUCCESS
         elif name in ticket["waitingList"]:
             oldTicket = FlightInfo(ticket)
             ticket["waitingList"].remove(name)
             newTicket = FlightInfo(ticket)
             revFlightInfo(oldTicket, newTicket)
-            uT.usrsHashTable().deleteFlightInfo(name, ticket)
+            # uT.usrsHashTable().deleteFlightInfo(name, ticket)
+            self.usrHashTable.deleteFlightInfo(name, ticket)
             return Const.SUCCESS
         else:
             return Const.USER_NOT_FOUND  # 未找到订单信息
@@ -1654,10 +1663,12 @@ class MainWindow(QtWidgets.QMainWindow):
                 msg_box.exec_()
                 return
 
-            usr = uT.usrsHashTable().get(self.usrname)[1]
+            # usr = uT.usrsHashTable().get(self.usrname)[1]
+            usr = self.usrHashTable.get(self.usrname)[1]
             usr["realName"] = realName
             usr["age"] = age
-            res = uT.usrsHashTable().revise(usr)
+            # res = uT.usrsHashTable().revise(usr)
+            res = self.usrHashTable.revise(usr)
             if res == Const.REVISE_FAILED:
                 msg_box = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Warning, "警告", "修改个人信息失败！")
                 msg_box.show()
@@ -1679,6 +1690,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def closeWindow(self):
         self.hide()
+        self.usrHashTable.updateJson()
         WriteDisk()
         self.close()
 
